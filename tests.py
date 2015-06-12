89c72a60fa5078416f07b94c1c6399d57c7fbd77
Brendan Smith <brendan.smith@gamer-network.net>
2015-06-12 14:36:41 +0100

    Added unittest stub

---
+++ tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+from datetime import datetime
+
+from analytics import StatsRange
+
+class TestStatsRange(unittest.TestCase):
+    
+    def test_date_formatting(self):
+        start = datetime(2015,06,12)
+        end = datetime(2015,06,13)
+        stats_range = StatsRange("Now", start, end)
+        self.assertEqual(stats_range.get_start(), "2015-06-12T00:00:00")
+        self.assertEqual(stats_range.get_end(), "2015-06-13T00:00:00")
+
+if __name__ == '__main__':
+    unittest.main()
