c0dc9bd5e34eea098a506c1ad646eee89c1c3cdc
fayebutler <faye_butler@hotmail.co.uk>
2016-02-26 10:42:16 +0000

    removing prints

--- reporting.py
+++ reporting.py
@@ -255,7 +255,6 @@ class TrafficSourceBreakdown(Report):
         logger = logging.getLogger('report')
         for site in config.TABLES.keys():
             site_ga_id = config.TABLES[site]
-            print site 
             site_data_available = analytics.data_available_for_site(site_ga_id, 
                 self.period.get_end())
             if site_data_available == False:
