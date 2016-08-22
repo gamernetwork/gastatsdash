312ef7f04f6f09f2bd3c353722b2c86de82e5a1b
fayebutler <faye_butler@hotmail.co.uk>
2016-08-22 16:33:26 +0100

    sending errors

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,13 @@
 import sqlite3, os, traceback
 
-
 from sqlite3 import OperationalError
 from datetime import date, datetime, timedelta
+import argparse
+import Statsdash.config as config
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import smtplib
 
 import Statsdash.config as config
 from Statsdash.report_schedule import reports
@@ -62,7 +67,7 @@ class RunLogger(object):
         )
         self.conn.commit()
 
-    def get_next_run(self, last_run, frequency, frequency_options={}):
+    def get_next_run(self, last_period_end, frequency, frequency_options={}):
         """
         When should the report run next.
         Returns a Date object
@@ -70,7 +75,7 @@ class RunLogger(object):
         today = date.today() - timedelta(days=1)
         now = datetime(today.year, today.month, today.day, 00, 00, 00, 01)  #returns now as datetime with time as 00 00 00 00001
         
-        if last_run.year == 1:
+        if last_period_end.year == 1:
             if frequency == 'DAILY' or frequency == "WOW_DAILY":
                 return now
             if frequency == 'WEEKLY':
@@ -84,22 +89,63 @@ class RunLogger(object):
                 return next_run
        
         if frequency == 'DAILY' or frequency == "WOW_DAILY":
-            #if last run was over 2 days ago, set to yesterday 
-            next_run = last_run + timedelta(days=1)
-            if (now - last_run).days >= 2:
+            #if last period end was over 2 days ago, set to yesterday 
+            next_run = last_period_end + timedelta(days=1)
+            if (now - last_period_end).days >= 2:
               self.override_data = True
         if frequency == 'WEEKLY':
-            next_run = last_run + timedelta(days=7)
+            next_run = last_period_end + timedelta(days=7)
         if frequency == 'MONTHLY':
             day = frequency_options.get("day")
-            next_run = add_one_month(last_run)
-            next_run = next_run.replace(day=day)
-            #why is this here 
-            #if next_run <= now + timedelta(days=1):
-               # next_run = add_one_month(next_run)    
+            #add one day to last_period_end to get the first day of next month period 
+            next_run = last_period_end + timedelta(days=1) 
+            #needs to run at the end of the next period, so add one month
+            next_run = add_one_month(next_run)
+            #make sure set to be correct day 
+            next_run = next_run.replace(day=day) 
             if (now - next_run).days >= 2:
               self.override_data = True
         return next_run
+
+
+class Errors():
+    errors = []
+    def get_errors(self):
+        """
+        Return list of current errors
+        """
+        return self.errors
+    
+    def add_error(self, error):
+        """
+        Add to list of errors
+        """
+        self.errors.append(error)
+        
+        
+    def send_errors(self):
+            """
+            Send html email using config parameters
+            """
+            msg = MIMEMultipart('alternative')
+            msg.set_charset('utf8')
+            msg['Subject'] = "Statsdash Errors"
+            msg['From'] = config.SEND_FROM
+            msg['To'] = config.ERROR_REPORTER
+            
+            message = ""
+            for error in self.errors:
+                message += error + "\n"
+            
+            text_part = MIMEText(message, 'plain')      
+            msg.attach(text_part)
+            
+            sender = smtplib.SMTP(config.SMTP_ADDRESS)
+            sender.sendmail(config.SEND_FROM, config.ERROR_REPORTER, msg.as_string())
+        
+            sender.quit()  
+            
+
 
 
 def _run():
@@ -127,6 +173,7 @@ def _run():
             if run_logger.override_data == True:
                 data_available = report.check_data_availability(override=True)
                 logger.warning("Overriding data availability and sending report anyway")
+                error_list.add_error("No data available, overriding for report %s between %s - %s " % (identifier, period.start_date, period.end_date))
             else:
                 data_available = report.check_data_availability()
             
@@ -134,8 +181,9 @@ def _run():
             print "%s next run: %s.  Data available: %s" % (identifier, next_run_date, data_available)
             if data_available:
                 try:
-                    html = report.generate_html()
-                    report.send_email(html)
+                    print "trying"
+                    #html = report.generate_html()
+                    #report.send_email(html)
                     run_datetime = datetime(year=report.period.end_date.year, 
                         month=report.period.end_date.month, 
                         day=report.period.end_date.day,
@@ -144,10 +192,46 @@ def _run():
                         second=0,
                         microsecond=1
                     )           
-                    run_logger.record_run(identifier, run_datetime)
+                    raise Exception("im an error")
+                    #run_logger.record_run(identifier, run_datetime)
                 except Exception:
-                	traceback.print_exc()
-                	continue
+                    print "Error in generating report %s" % identifier
+                    error_list.add_error("Error in generating report %s : \n %s" % (identifier, traceback.format_exc()))
+                    traceback.print_exc()
+                    continue
+
+def dry_run():
+    """
+    Run the scheduler and print what would happen but don't actually run or record reports
+    """
+    
+    run_logger = RunLogger()
+    
+    for config in reports:
+        identifier = config['identifier']
+        frequency = config['frequency']
+        frequency_options = config.get('frequency_options', {})
+        report_class = config['report']
+        last_run = run_logger.get_last_run(identifier)
+        next_run_date = run_logger.get_next_run(last_run, frequency, frequency_options).date()
+        today = date.today()
+        needs_run = next_run_date <= today
+        print "%s next run: %s.  Needs run: %s" % (identifier, next_run_date, needs_run)
+        if needs_run:
+            period = utils.StatsRange.get_period(next_run_date, frequency)
+            report = config["report"](config["sites"], period, config["recipients"], config["frequency"], config["subject"])  
+            print "last run %s" %(last_run)
+            print "period for run: %s - %s" % (period.start_date, period.end_date)
+            if run_logger.override_data == True:
+                data_available = report.check_data_availability(override=True)
+                print "Override data"
+            else:
+                data_available = report.check_data_availability()
+            
+            run_logger.override_data = False
+            print "%s next run: %s.  Data available: %s" % (identifier, next_run_date, data_available)    
+
+  
 
 
 def run_schedule():
@@ -159,5 +243,14 @@ def run_schedule():
     print "** Finished run at %s" % datetime.now().isoformat()
 
 if __name__ == '__main__':
-
-    run_schedule()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--test", help="test run", action="store_true")
+    args = parser.parse_args()
+    error_list = Errors()
+    
+    if args.test == False:
+       run_schedule()
+    elif args.test == True:
+        dry_run()
+    
+    error_list.send_errors()
